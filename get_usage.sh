4198e5600daeaa4fc918c936776363ffaebcf0a1
Robin Guignard-Perret <yxdunc@users.noreply.github.com>
2019-09-18 12:01:35 +0200

    ADD colors

--- get_usage.sh
+++ get_usage.sh
@@ -1,3 +1,13 @@
+green () {
+	text=$1
+	echo "\e[32m${text}\033[39m"
+}
+
+red () {
+	text=$1
+	echo "\e[31m${text}\033[39m"
+}
+
 for x in /proc/asound/card*/pcm*/sub*/status; do
 	state=$(cat $x)
 	alsa_device=$(dirname ${x#*/asound/})
@@ -5,9 +15,9 @@ for x in /proc/asound/card*/pcm*/sub*/status; do
 	if [ "$state" != "closed" ]; then
 		owner_pid=$(cat $x | grep owner_pid | sed -r "s/owner_pid +: +*([0-9]*).*/\1/")
 		owner_name=$(ps | grep ${owner_pid} | grep -v grep)
-		echo "[RUNNING] ${alsa_device} ( ${io} )"
+		printf "[$(green RUNNING)] ${alsa_device} ( ${io} )\n"
 		echo "	owner: $owner_name"
 	else
-		echo "[X] ${alsa_device} ( ${io} )"
+		printf "[$(red X)] ${alsa_device} ( ${io} )\n"
 	fi
 done
